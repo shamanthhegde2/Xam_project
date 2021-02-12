694b976fd9657ba543085e55408c0c116578078c
shamanthhegde2 <shamanthhegde12@gmail.com>
2021-02-12 19:00:28 +0530

    feat(added showpassword and validation for password)

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ let login = document.querySelector('.login-part')
 let signup = document.querySelector('.signup-part')
 let login_link = document.querySelector('.login-link')
 let signup_link = document.querySelector('.signup-link')
+
+function show_password() {
+  let x = document.getElementById('login_Password')
+  if (x.type === 'password') {
+    x.type = 'text'
+  } else {
+    x.type = 'password'
+  }
+}
 signup.style.display = 'none'
 
 function login_switch() {
@@ -13,3 +22,22 @@ function signup_switch() {
   signup.style.display = 'flex'
   login.style.display = 'none'
 }
+
+let validation = document.querySelector('.need_validation')
+
+validation.addEventListener('submit', function (event) {
+  let x = document.querySelector('#sign_Password')
+  let y = document.querySelector('#ConfirmPassword')
+  if (x.value != y.value) {
+    event.preventDefault()
+    event.stopPropagation()
+    x.classList.add('red_border')
+    y.classList.add('red_border')
+    x.value = ''
+    y.value = ''
+    setTimeout(function () {
+      x.classList.remove('red_border')
+      y.classList.remove('red_border')
+    }, 2000)
+  }
+})
